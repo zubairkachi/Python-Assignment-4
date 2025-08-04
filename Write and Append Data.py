2500f36bb33ad00ce2dae4b4b52d9c73a28440e4
zubairkachi <kachizubairhussain0@gmail.com>
2025-08-04 13:40:58 +0530

    Add files via upload

--- Write and Append Data.py
+++ Write and Append Data.py
@@ -1,12 +1,21 @@
-write=str(input("Enter text to write to the file: "))
 file=open("output.txt","w")
+write=str(input("Enter text to write to the file: "))
 writing_file=file.write(write+"\n")
 file.close()
+if writing_file==len(write)+1:
+    print("Data successfully written to output.txt")
+else:
+    print("Data successfully not written to output.txt")
 file=open("output.txt","a")
 append=str(input("Enter additional text to append: "))
 appending_file=file.write(append)
 file.close()
+if appending_file==len(append):
+    print("Data successfully appended")
+else:
+    print("Data successfully not appended")
 file=open("output.txt","r")
 reading_file=file.read()
+print("Final contents of output.txt:")
 print(reading_file)
 file.close()
